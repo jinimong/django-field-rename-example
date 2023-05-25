bf84ef9a22e34d48c03039e7e21e6261ebfc11b8
jwkim <phoongdeng@gmail.com>
2023-05-25 23:53:06 +0900

    Rename field name : poeple -> member

--- clubs/models.py
+++ clubs/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 
-class People(models.Model):
+class Member(models.Model):
     name = models.CharField(max_length=100)
     age = models.PositiveSmallIntegerField()
 
 
 class Club(models.Model):
     name = models.CharField(max_length=100)
-    poeples = models.ManyToManyField(People, related_name="clubs")
+    members = models.ManyToManyField(Member, related_name="clubs")
 
 
 class Post(models.Model):
     text = models.TextField
-    people = models.ForeignKey(People, on_delete=models.CASCADE, related_name="posts")
+    member = models.ForeignKey(Member, on_delete=models.CASCADE, related_name="posts")
--- clubs/tests.py
+++ clubs/tests.py
@@ -3,5 +3,5 @@ from .models import Club
 
 
 class ModelTest(TestCase):
-    def test_club_peoples_m2m_field_table(self):
-        self.assertEqual(Club.poeples.through._meta.db_table, "clubs_club_poeples")
+    def test_club_members_m2m_field_table(self):
+        self.assertEqual(Club.members.through._meta.db_table, "clubs_club_members")
