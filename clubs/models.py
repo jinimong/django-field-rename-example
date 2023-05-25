0598e11c40f5219b3cff4e7594c85be7eadbe59e
jwkim <phoongdeng@gmail.com>
2023-05-25 23:41:03 +0900

    Add club app models

--- clubs/models.py
+++ clubs/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class People(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField()
+
+
+class Club(models.Model):
+    name = models.CharField(max_length=100)
+    poeples = models.ManyToManyField(People, related_name="clubs")
+
+
+class Post(models.Model):
+    text = models.TextField
+    people = models.ForeignKey(People, on_delete=models.CASCADE, related_name="posts")
--- clubs/tests.py
+++ clubs/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Club
 
-# Create your tests here.
+
+class ModelTest(TestCase):
+    def test_club_peoples_m2m_field_table(self):
+        self.assertEqual(Club.poeples.through._meta.db_table, "clubs_club_poeples")
